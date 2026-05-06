d11ff5d1a3677472fc9c2afe43366d90b3c8143c
NeerajCodz <neerajcodz@gmail.com>
2026-05-06 22:32:45 +0530

    bench: add columnar acceleration benchmark

---
+++ benches/accel/columnar_filter_bench.rs
@@ -0,0 +1,23 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use trident::accel::{Accelerator, CpuAccelerator};
+
+fn bench_columnar_filter(c: &mut Criterion) {
+    let accel = CpuAccelerator;
+    let values: Vec<Vec<u8>> = (0..100_000)
+        .map(|i| {
+            if i % 10 == 0 {
+                b"target".to_vec()
+            } else {
+                format!("value-{i}").into_bytes()
+            }
+        })
+        .collect();
+    let refs: Vec<&[u8]> = values.iter().map(Vec::as_slice).collect();
+
+    c.bench_function("accel_columnar_eq_offsets", |b| {
+        b.iter(|| black_box(accel.columnar_eq_offsets(black_box(&refs), black_box(b"target"))))
+    });
+}
+
+criterion_group!(benches, bench_columnar_filter);
+criterion_main!(benches);
